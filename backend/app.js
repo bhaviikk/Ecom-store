b0af63b5b04cfed58297786edac6a76949c9fdc6
bhaviikk <thankibhavik1@gmail.com>
2022-07-23 10:02:19 +0530

    cors fixed

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,6 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const path = require("path");
-const cors = require("cors");
 
 const errorMiddleware = require("./middleware/error");
 
@@ -12,7 +11,7 @@ const errorMiddleware = require("./middleware/error");
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({ path: "backend/config/config.env" });
 }
-app.use(cors());
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
